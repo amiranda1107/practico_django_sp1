d3d06c5fe5decceff4e446c5a856cb80405cc1b0
Adrián Javier <60413797+amiranda1107@users.noreply.github.com>
2023-08-29 22:20:28 -0400

    Curso de Django con faztweb.com

---
+++ djangoproject/myapp/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import render, redirect
+from .forms import CreateNewTask
+
+# Create your views here.
+def index(request):
+    title = 'Django Course!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
+
+def about(request):
+    username = 'AJ'
+    return render(request, 'about.html', {
+        'username': username
+    })
+
+def hello(request, username):
+    return HttpResponse("Hello, %s" % username)
+
+def projects(request):
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
+
+def tasks(request):
+    #task = get_object_or_404(Task, id=id)
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
+    
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': CreateNewTask()
+    }) 
+    else:
+        Task.objects.create(title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('/tasks/') 
+    
+        
